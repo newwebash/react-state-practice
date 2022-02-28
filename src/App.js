096e964c36fc7a2a412d4b2951aed8c060f9fa84
newwebash <newtona2011@gmail.com>
2022-02-28 12:07:51 -0500

    Move count and score to state and update as needed

--- src/App.js
+++ src/App.js
@@ -6,10 +6,38 @@ const value1 = Math.floor(Math.random() * 100);
 const value2 = Math.floor(Math.random() * 100);
 const value3 = Math.floor(Math.random() * 100);
 const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
 
 class App extends Component {
+  state = {
+    numQuestions: 0,
+    numCorrect: 0,
+  }
+  updateNumQuestions = () => {
+    this.setState((currentState) => {
+      return currentState.numQuestions = currentState.numQuestions + 1;
+    })
+  }
+  updateNumCorrect = () => {
+    this.setState((currentState) => {
+      return currentState.numCorrect = currentState.numCorrect + 1;
+    })
+  }
+  sumNums = (numArr) => {
+    let total = 0;
+    numArr.forEach((num) => {
+      total = total + num;
+    })
+    return total;
+  }    
+  submitAnswer = (userSubmission) => {
+    // Get correct answer, true or false
+    const correctAnswer = this.sumNums([value1, value2, value3]) === proposedAnswer;   
+
+    if (userSubmission === correctAnswer) {
+      this.updateNumCorrect();
+    }
+    this.updateNumQuestions();
+  }
   render() {
     return (
       <div className="App">
@@ -22,10 +50,10 @@ class App extends Component {
           <div className="equation">
             <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
           </div>
-          <button>True</button>
-          <button>False</button>
+          <button onClick={() => this.submitAnswer(true)}>True</button>
+          <button onClick={() => this.submitAnswer(false)}>False</button>
           <p className="text">
-            Your Score: {numCorrect}/{numQuestions}
+            Your Score: {this.state.numCorrect}/{this.state.numQuestions}
           </p>
         </div>
       </div>
